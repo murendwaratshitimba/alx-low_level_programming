712b1db3cce4248cb5c7d7cffdbd2a14c3d68aaf
Murendwaratshitimba <murendwaratshitimba@gmail.com>
2022-10-13 13:24:11 +0200

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly prints exactly and that piece 
+ * main - prints exactly prints exactly and that piece
  * of art is useful" - Dora Korpar, 2015-10-19
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
