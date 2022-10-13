ee4ecafa5c4167bffa9fe02cc85c81ec2534410c
Murendwaratshitimba <murendwaratshitimba@gmail.com>
2022-10-13 11:13:00 +0200

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+// preprocessor
+
+#include <stdio.h>
+
+/**
+  * This is the main method that displays text 
+  * using puts function
+  *
+  * Return: int() 0
+  */
+
+int main()
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0;
+}
