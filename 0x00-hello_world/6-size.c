7c83d928256b5e5c53e6681ec0c594ba105fe53c
Murendwaratshitimba <murendwaratshitimba@gmail.com>
2022-10-13 12:30:32 +0200

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 
 /**
-  * displays the sizes of char, int, long int,
-  * long long int, float data types in bytes
-  *
-  *
-  * returns an interger value of 0
-  */
-
+ * main - prints the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-	
 
-char character;
-int integer;
-long int longInteger;
-long long int longLongInteger;
-float Float;
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
 
-printf("Size of a char: %lu byte(s)\n", sizeof(character));
-printf("Size of an int: %lu byte(s)\n", sizeof(integer));
-printf("Size of a long int: %lu byte(s)\n", sizeof(longInteger));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongInteger));
-printf("Size of a float: %lu byte(s)\n", sizeof(Float));
-	
-return 0;
 }
