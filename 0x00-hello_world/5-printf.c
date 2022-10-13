03740dc6a049ccdb782b3d949bce62601c9d72d0
Murendwaratshitimba <murendwaratshitimba@gmail.com>
2022-10-13 12:16:41 +0200

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
-printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+{
+ 
+printf("%s\n","with proper grammar, but the outcome is a piece of art,");
 return (0);
+
 }
 
