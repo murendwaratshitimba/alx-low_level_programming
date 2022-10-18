968eff253e9d162af7bef27422f830d125d4ea08
Murendwaratshitimba <murendwaratshitimba@gmail.com>
2022-10-18 19:50:06 +0200

    first_number + second_number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @first_number: first numeric input
+ * @second_number: second numeric input
+ *
+ * Return: sum of first_number and second_number
+ */
+int add(int first_number, int second_number)
+
+{
+
+	return (first_number + second_number);
+
+}
