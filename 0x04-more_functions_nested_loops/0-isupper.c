456d63e6526b0b347f702f1a8e782a505f93dfac
Murendwaratshitimba <murendwaratshitimba@gmail.com>
2022-10-20 21:42:25 +0200

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ *@alphabet: print int
+ * Return: 0 or 1.
+ */
+
+int _isupper(int alphabet)
+{
+if (alphabet >= 'A' && alphabet <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
