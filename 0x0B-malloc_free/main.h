131da6dda93a44908a563f8aeb30d68ad76f6ca3
Murendwaratshitimba@Gmail.Com <murendwaratshitimba@gmail.com>
2022-11-07 11:25:02 +0200

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif /* MAIN_H */
