ef3ea57ed0063cad5a2afcafdcfa981ae8c37ada
Murendwaratshitimba@Gmail.Com <murendwaratshitimba@gmail.com>
2022-11-10 22:05:54 +0200

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
 * _putchar - writes the character c to stdout
@@ -7,7 +8,6 @@
 * Return: On success 1.
 * On erro, -1 is returned, and errno is set appropriately.
 */
-
 int _putchar(char character)
 
 {
